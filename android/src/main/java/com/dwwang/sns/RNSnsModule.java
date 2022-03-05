b9d1a62c213046ee7f36bfc2fa8712d40f92124b
dwwang <daivdggdev@163.com>
2022-03-05 11:20:45 +0800

    fix: 编译问题

--- android/src/main/java/com/dwwang/sns/RNSnsModule.java
+++ android/src/main/java/com/dwwang/sns/RNSnsModule.java
@@ -44,7 +44,7 @@ public class RNSnsModule extends ReactContextBaseJavaModule {
         // }
 
         @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        public void onActivityResult(int requestCode, int resultCode, Intent data){
             super.onActivityResult(requestCode, resultCode, data);
             UMShareAPI.get(mContext).onActivityResult(requestCode, resultCode, data);
         }
